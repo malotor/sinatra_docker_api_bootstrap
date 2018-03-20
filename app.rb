9a81f4a86ca7e48cbac193b35fd91730512f31e9
Manel <malotor@gmail.com>
2018-03-20 22:15:37 +0100

    Refactor: create class variable article

--- app.rb
+++ app.rb
@@ -8,7 +8,6 @@ Dir["#{current_dir}/models/*.rb"].each { |file| require file }
 
 class MyApp < Sinatra::Base
 
-
     before do
         content_type 'application/json'
     end
@@ -22,53 +21,48 @@ class MyApp < Sinatra::Base
     end
 
     get "/articles/:id" do
-        begin
-            json Article.find(params[:id])
-        rescue
-            halt_if_not_found
-        end
+        halt_if_not_found
+        json @article
     end
 
     post '/articles' do
-        article = Article.new(required_params)
-        status 422 unless article.save
-
-        response.headers['Location'] = "#{base_url}/articles/#{article.id}"
+        @article = Article.new(required_params)
+        status 422 unless @article.save
+        response.headers['Location'] = "#{base_url}/articles/#{@article.id}"
         status 201
     end
 
 
     delete '/articles/:id' do
-        begin
-            Article.destroy(params[:id])
-            status 204
-        rescue
-            halt_if_not_found
-        end
+        halt_if_not_found
+        @article.destroy
+        status 204
     end
 
     private
 
-        helpers do
-
-            def base_url
-                @base_url ||= "#{request.env['rack.url_scheme']}://{request.env['HTTP_HOST']}"
-            end
-
-            def required_params
-                error 400, 'Invalid data' unless params[:title]
-                error 400, 'Invalid data' unless params[:content]
-
-                { title: params[:title], content: params[:content] }
-            end
-
-            def error(code, message)
-                halt code, { code: code, message: message }.to_json
-            end
-
-            def halt_if_not_found
-                error 404, 'Article doesn`t exists'
-            end
+        def get_article
+          @article ||= Article.find(params[:id])
         end
+
+        def base_url
+            @base_url ||= "#{request.env['rack.url_scheme']}://{request.env['HTTP_HOST']}"
+        end
+
+        def required_params
+            error 400, 'Invalid data' unless params[:title]
+            error 400, 'Invalid data' unless params[:content]
+
+            { title: params[:title], content: params[:content] }
+        end
+
+        def error(code, message)
+            halt code, { code: code, message: message }.to_json
+        end
+
+        def halt_if_not_found
+            error 404, 'Article doesn`t exists' unless get_article
+        end
+
 
 end
