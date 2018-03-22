7984e5a52b534ee606889433fc8fc22aafc7bc96
Manel <mlopezt@prosodie.com>
2018-03-22 11:44:43 +0100

    Add publish articles test

--- models/Article.rb
+++ models/Article.rb
@@ -1,4 +1,29 @@
 class Article < ActiveRecord::Base
+
+    attr_reader :published,:published_on,:title,:content
+
     validates :title, presence: true, length: { maximum: 100 }
     validates :content, presence: true
+
+    def initialize(title, content)
+      @title = title
+      @content = content
+      @published  = false
+      @likes = 0
+    end
+
+    def have_been_published?
+      @published
+    end
+
+    def publish
+      raise ArticlesHasAlreadyPublishedError if @published
+      @published = true
+      @published_on = Time.now
+    end
+
+
+end
+
+class ArticlesHasAlreadyPublishedError < StandardError
 end
