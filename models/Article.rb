e54e4887050698627156c17831ae0f4ad3107da5
Manel <mlopezt@prosodie.com>
2018-03-23 14:49:45 +0100

    Temporal commit

--- models/Article.rb
+++ models/Article.rb
@@ -5,11 +5,12 @@ class Article < ActiveRecord::Base
     validates :title, presence: true, length: { maximum: 100 }
     validates :content, presence: true
 
-    def initialize(title, content)
-      @title = title
+    def initialize(attributes = nil)
+      super(attributes)
       @content = content
       @published  = false
       @likes = 0
+
     end
 
     def have_been_published?
